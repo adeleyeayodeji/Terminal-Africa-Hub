2666bd8c1979737e6866236d804c9f47bf9b367b
adeleyeayodeji <biodunhi@gmail.com>
2024-06-05 10:54:03 +0100

    Added 404 page

--- 404.php
+++ 404.php
@@ -1,17 +1,25 @@
 <?php
+
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Terminal Africa Hub
+ */
+
 get_header();
 ?>
 
-<header class="page-header alignwide">
-    <h1 class="page-title"><?php esc_html_e('Nothing here', 'terminal-africa-hub'); ?></h1>
-</header><!-- .page-header -->
-
-<div class="error-404 not-found default-max-width">
-    <div class="page-content">
-        <p><?php esc_html_e('It looks like nothing was found at this location. Maybe try a search?', 'terminal-africa-hub'); ?></p>
-        <?php get_search_form(); ?>
-    </div><!-- .page-content -->
-</div><!-- .error-404 -->
+<div class="container py-5">
+    <div class="row justify-content-center py-5">
+        <div class="col-md-12 text-center py-5">
+            <span class="display-1 d-block">404</span>
+            <div class="mb-4 lead">The page you are looking for was not found.</div>
+            <a href="<?php echo esc_url(home_url()); ?>" class="btn btn-link">Back to Home</a>
+        </div>
+    </div>
+</div>
 
 <?php
 get_footer();
